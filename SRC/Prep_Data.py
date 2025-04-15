d3f35d950ca3544f746a22fee1b9f0326bf5be27
TylerG14 <tyler.geuens@gmail.com>
2025-04-15 10:06:21 +0200

    Explained how the data split works and how to use the testing and training data

--- SRC/Prep_Data.py
+++ SRC/Prep_Data.py
@@ -16,9 +16,13 @@ def explore_student_data(df):
     print("\nGradeClass distribution:")
     print(df['GradeClass'].value_counts())
 
+#seperates the data int training and test data.
+#X_train contains the features for the training data and X_test contains the testing features and both Y_train and Y_test shows the gpa values. 
+#Removed GPA from the training data to ensure the that we can predict the GPA values and not just use them
+#to use the training data use X_train instead of df where normal
 def prepare_data_for_training(df, test_size=0.2, random_state=42):
-    X = df.drop(columns=["GradeClass"])  # Features
-    y = df["GradeClass"]                 # Target
+    X = df.drop(columns=["GPA"])  # Features
+    y = df["GPA"]                 # Target
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=test_size, stratify=y, random_state=random_state
     )
