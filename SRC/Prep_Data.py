11a07402bf2e5d98ac74de2de53456b4b887434c
TylerG14 <tyler.geuens@gmail.com>
2025-04-17 13:44:36 +0200

    Completed step 8. impletented Feature engineering

--- SRC/Prep_Data.py
+++ SRC/Prep_Data.py
@@ -31,4 +31,25 @@ def prepare_data_for_training(df, test_size=0.2, random_state=42):
     print(f"\nData split: {len(X_train)} training and {len(X_test)} testing samples.")
     return X_train, X_test, y_train, y_test
 
+def engineer_features(df):
+    #Creates the features "the input or x values" for the model to use in prediction of GradeClass
 
+    # Combine all activity participation into a single score
+    df['TotalActivities'] = df['Extracurricular'] + df['Sports'] + df['Music'] + df['Volunteering']
+    print("Added TotalActivities (sum of activities).")
+    
+    # Group absences into categories: Low, Medium, High
+    bins = [-1, 9, 19, 29]  # -1 to include 0
+    labels = ['Low', 'Medium', 'High']
+    df['AbsencesBinned'] = pd.cut(df['Absences'], bins=bins, labels=labels)
+    print("Added AbsencesBinned (Low, Medium, High).")
+    
+     # Convert absence categories to numeric values
+    df['AbsencesBinned'] = df['AbsencesBinned'].cat.codes
+    print("Converted AbsencesBinned to numerical codes.")
+    
+    # Create a ratio of study time to absences (+1 to avoid division by zero
+    df['StudyTimePerAbsence'] = df['StudyTimeWeekly'] / (df['Absences'] + 1)
+    print("Added StudyTimePerAbsence (StudyTimeWeekly / (Absences + 1)).")
+    
+    return df
