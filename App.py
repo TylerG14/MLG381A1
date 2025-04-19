2fcbec40170017d79216c52f88c3df34b98bbbc7
MarcoReiners <marcoreiners816@gmail.com>
2025-04-19 22:27:16 +0200

    updated app.py

--- App.py
+++ App.py
@@ -57,8 +57,7 @@ app.layout = html.Div([
 
 # Run app
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
     print("Launching Dash app...")
-    try:
-        app.run(debug=True, host='127.0.0.1', port=8050)
-    except Exception as e:
-        print("Failed to start server:", e)
+    app.run(debug=True, host="0.0.0.0", port=port)
